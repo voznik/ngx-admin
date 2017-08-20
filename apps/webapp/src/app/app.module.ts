1bd3c5a573ae1bbdd0a848f24834bb6c45c2ef44
Brannon N. Darby II <bdarby86@gmail.com>
2017-08-20 15:47:58 -0400

    refactor(core): move core imports to CoreModule and clean up AppModule

--- apps/webapp/src/app/app.module.ts
+++ apps/webapp/src/app/app.module.ts
@@ -1,22 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule, ModuleWithProviders } from '@angular/core'
-import { StoreModule } from '@ngrx/store'
-import { EffectsModule } from '@ngrx/effects'
-import { StoreDevtoolsModule } from '@ngrx/store-devtools'
-
-import { environment } from '../environments/environment'
-import {
-  AuthEffects,
-  AuthReducer,
-  AdminReducer,
-  UserEffects,
-  RoleEffects,
-  ControlEffects,
-} from './state'
 
 import { CoreModule } from './core.module'
-import { NgxUiModule, NgxUiService } from './ui'
-
 import { AppComponent } from './app.component'
 import { AppRoutingModule } from './app.routing'
 
@@ -25,20 +10,9 @@ import { AppRoutingModule } from './app.routing'
   imports: [
     BrowserModule,
     CoreModule,
-    StoreModule.forRoot({
-      admin: AdminReducer,
-      auth: AuthReducer,
-    }),
-    EffectsModule.forRoot([
-      UserEffects,
-      RoleEffects,
-      ControlEffects,
-      AuthEffects,
-    ]),
-    !environment.production ? StoreDevtoolsModule.instrument() : [],
     AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
--- apps/webapp/src/app/core.module.ts
+++ apps/webapp/src/app/core.module.ts
@@ -1,16 +1,39 @@
-import {
-  ModuleWithProviders,
-  NgModule,
-  Optional,
-  SkipSelf,
-} from '@angular/core'
+import { ModuleWithProviders, NgModule, Optional, SkipSelf } from '@angular/core'
 import { SDKBrowserModule, LoopBackConfig } from '@ngx-plus/ngx-sdk'
+import { StoreModule } from '@ngrx/store'
+import { EffectsModule } from '@ngrx/effects'
+import { StoreDevtoolsModule } from '@ngrx/store-devtools'
+
+import { environment } from '../environments/environment'
+import {
+  AuthEffects,
+  AuthReducer,
+  AdminReducer,
+  UiReducer,
+  UserEffects,
+  RoleEffects,
+  ControlEffects,
+} from './state'
 
 import { NgxUiModule } from './ui'
 
 @NgModule({
-  imports: [SDKBrowserModule.forRoot(), NgxUiModule.forRoot()],
-  providers: [],
+  imports: [
+    SDKBrowserModule.forRoot(),
+    NgxUiModule.forRoot(),
+    StoreModule.forRoot({
+      admin: AdminReducer,
+      auth: AuthReducer,
+      ui: UiReducer,
+    }),
+    EffectsModule.forRoot([
+      UserEffects,
+      RoleEffects,
+      ControlEffects,
+      AuthEffects,
+    ]),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+  ],
   exports: [SDKBrowserModule, NgxUiModule],
 })
 export class CoreModule {
